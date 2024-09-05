e4febdaa16900049d5ff4739abb6e8f1f686bffe
codeskavy <kanikaverma0510@gmail.com>
2024-09-06 01:49:15 +0530

    css

--- src/pages/NewsGrid.js
+++ src/pages/NewsGrid.js
@@ -12,10 +12,10 @@ import img2 from '../assets/2.jpg';
 const NewsGrid = () => {
     return (
         <div className="photo-column-grid">
-          <div className="photo-item1">
+          <div className="photo-item">
             <img src={img1} alt="Photo 1" />
           </div>
-          <div className="photo-item2">
+          <div className="photo-item">
             <img src={img2} alt="Photo 2" />
           </div>
         </div>
